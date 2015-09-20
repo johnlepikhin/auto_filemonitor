3c2f86df3cc474e4ea9be86d023f193d9447da0f
Evgenii Lepikhin <johnlepikhin@gmail.com>
2015-09-20 21:08:14 +0300

    Improvements

--- src/filemonitor.ml
+++ src/filemonitor.ml
@@ -1,27 +1,39 @@
 
 type t = {
   name : string;
-  stats : Unix.stats;
+  stats : Unix.stats option;
 }
 
 let init name = {
   name;
-  stats = Unix.stat name;
+  stats = Some (Unix.stat name);
 }
 
 let update t = init t.name
 
 let differs t1 t2 =
   let open Unix in
-  let t1 = t1.stats in
-  let t2 = t2.stats in
-  t1.st_mtime <> t2.st_mtime
+  match (t1.stats, t2.stats) with
+  | None, None -> false
+  | Some _, None
+  | None, Some _ -> true
+  | Some t1, Some t2 ->
+     t1.st_mtime <> t2.st_mtime
      || t2.st_size <> t2.st_size
      || t2.st_ino <> t2.st_ino
 
 let changed t =
   differs t (update t)
 
-let write_t _ _ = ()
-let read_t _ _ = raise Exit
+let write_t ob _ =
+  ()
+(*
+  Bi_outbuf.add_string ob "\"<unused>\""
+*)
+
+let read_t _ _ =
+  {
+    name = "";
+    stats = None;
+  }
     
